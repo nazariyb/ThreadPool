f2c191c49645a0b8c1d7ed65fd8d0f789665b9ba
nazariyb <bachynsky@ucu.edu.ua>
2019-05-10 01:24:00 +0300

    add simple thread pool

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,1 +1,28 @@
 #include "thread_pool.h"
+
+void thread_pool::worker_thread ()
+{
+    while (!done) {
+        std::function<void ()> task;
+        if (work_queue.try_pop(task)) {
+            task();
+        } else {
+            std::this_thread::yield();
+        }
+    }
+}
+
+thread_pool::thread_pool () :
+        done(false), joiner(threads)
+{
+    unsigned const thread_count = std::thread::hardware_concurrency();
+    try {
+        for (unsigned i = 0; i < thread_count; ++i) {
+            threads.emplace_back(&thread_pool::worker_thread, this);
+        }
+    }
+    catch (...) {
+        done = true;
+        throw;
+    }
+}
--- thread_pool.h
+++ thread_pool.h
@@ -2,10 +2,35 @@
 #define TREAD_POOL_THREAD_POOL_H
 
 
+#include <atomic>
+#include <thread>
+#include <functional>
+#include <vector>
+#include "thread_safe_queue.h"
+
 class thread_pool
     {
+private:
+    std::atomic_bool done;
+    thread_safe_queue<std::function<void ()>> work_queue;
+    std::vector<std::thread> threads;
+    join_threads joiner{};
 
+    void worker_thread ();
+
+public:
+    thread_pool ();
+
+    ~thread_pool ()
+    {
+        done = true;
+    }
+
+    template<typename FunctionType>
+    void submit (FunctionType f)
+    {
+        work_queue.push(std::function<void ()>(f));
+    }
     };
-
 
 #endif //TREAD_POOL_THREAD_POOL_H
