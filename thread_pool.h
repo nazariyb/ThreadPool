b40baf741cd81dddc892c8afe1fab6b636fb6762
nazariyb <bachynsky@ucu.edu.ua>
2019-05-09 21:23:05 +0300

    initial commit

---
+++ thread_pool.h
@@ -0,0 +1,11 @@
+#ifndef TREAD_POOL_THREAD_POOL_H
+#define TREAD_POOL_THREAD_POOL_H
+
+
+class thread_pool
+    {
+
+    };
+
+
+#endif //TREAD_POOL_THREAD_POOL_H
---
+++ thread_safe_queue.h
@@ -0,0 +1,11 @@
+#ifndef TREAD_POOL_THREAD_SAFE_QUEUE_H
+#define TREAD_POOL_THREAD_SAFE_QUEUE_H
+
+
+class thread_safe_queue
+    {
+
+    };
+
+
+#endif //TREAD_POOL_THREAD_SAFE_QUEUE_H
