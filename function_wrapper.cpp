b65a46dff1955c04a7f945d53b39a6c461426e83
nazariyb <bachynsky@ucu.edu.ua>
2019-05-10 02:04:23 +0300

    add function wrapper

---
+++ function_wrapper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 3naza on 5/10/2019.
+//
+
+#include "function_wrapper.h"
---
+++ function_wrapper.h
@@ -0,0 +1,15 @@
+//
+// Created by 3naza on 5/10/2019.
+//
+
+#ifndef TREAD_POOL_FUNCTION_WRAPPER_H
+#define TREAD_POOL_FUNCTION_WRAPPER_H
+
+
+class function_wrapper
+    {
+
+    };
+
+
+#endif //TREAD_POOL_FUNCTION_WRAPPER_H
