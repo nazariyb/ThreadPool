01b3cf0b7cb998681839d91c09f3849d4ee5dfc0
nazariyb <bachynsky@ucu.edu.ua>
2019-05-10 05:29:58 +0300

    add work stealing queue

---
+++ work_stealing_queue.h
@@ -0,0 +1,15 @@
+//
+// Created by 3naza on 5/10/2019.
+//
+
+#ifndef TREAD_POOL_WORK_STEALING_QUEUE_H
+#define TREAD_POOL_WORK_STEALING_QUEUE_H
+
+
+class work_stealing_queue
+    {
+
+    };
+
+
+#endif //TREAD_POOL_WORK_STEALING_QUEUE_H
